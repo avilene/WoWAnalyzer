866004d876f7ba7323dc0e81e115bd7e8b2cc883
Aveline Estié <aa.estie@gmail.com>
2024-09-20 14:19:19 +0200

    add TWW flasks

--- src/common/SPELLS/thewarwithin/index.ts
+++ src/common/SPELLS/thewarwithin/index.ts
@@ -4,7 +4,8 @@ import Embellishments from './embellishments';
 import Others from './others';
 import Raids from './raids';
 import Trinkets from './trinkets';
+import Flasks from './flasks';
 
-const spells = safeMerge(Enchants, Embellishments, Others, Raids, Trinkets);
+const spells = safeMerge(Enchants, Embellishments, Others, Raids, Trinkets, Flasks);
 
 export default spells;
